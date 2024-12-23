f0b2e9028addf84f26a0fd900c09b891e7506ecb
Shadowpatriot9 <gscovil9@gmail.com>
2024-12-22 20:36:06 -0700

    -added status indicator for s9 -adjusted mobile design for s9

--- bin/projects/s9/script.js
+++ bin/projects/s9/script.js
@@ -9,3 +9,34 @@ document.getElementById('gs-btn').addEventListener('click', function () {
   });
 
 /////////////////////////////
+
+async function checkServerStatus() {
+  try {
+      // Fetch status.json from the server
+      const response = await fetch('http://107.2.138.206/status.json');
+      
+      if (!response.ok) {
+          throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      const statusIndicator = document.getElementById('status-indicator');
+  
+      if (data.status === 'online') {
+          statusIndicator.innerHTML = '●';
+          statusIndicator.classList.add('emoji');
+          monitorStatus.textContent = 'Online';
+          statusIndicator.textContent = 'Online';
+      } else {
+          statusIndicator.innerHTML = '○';
+          monitorStatus.textContent = 'Offline';
+      }
+  } catch (error) {
+      document.getElementById('status-indicator-1').innerHTML = '○';
+      document.getElementById('status-indicator-1').classList.add('emoji');
+      document.getElementById('status-indicator-2').textContent = 'Error: Unable to reach server';
+    
+  }
+}
+
+window.onload = checkServerStatus;
