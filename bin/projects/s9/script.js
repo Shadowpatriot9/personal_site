bf35a77b982fda122dbbf4b74c3a8ee0fdfc8a8d
Shadowpatriot9 <gscovil9@gmail.com>
2024-12-22 22:45:27 -0700

    -fixed status indicator for s9 -minor adjustment to hover for project grid in main -fixed link to gh for site

--- bin/projects/s9/script.js
+++ bin/projects/s9/script.js
@@ -9,6 +9,8 @@ document.getElementById('gs-btn').addEventListener('click', function () {
   });
 
 /////////////////////////////
+// Monitoring Dashboard
+/////////////////////////////
 
 async function checkServerStatus() {
   try {
@@ -23,20 +25,18 @@ async function checkServerStatus() {
       const statusIndicator = document.getElementById('status-indicator');
   
       if (data.status === 'online') {
-          statusIndicator.innerHTML = '●';
-          statusIndicator.classList.add('emoji');
-          monitorStatus.textContent = 'Online';
-          statusIndicator.textContent = 'Online';
+          statusIndicator.textContent = '● Online';
       } else {
-          statusIndicator.innerHTML = '○';
-          monitorStatus.textContent = 'Offline';
+          statusIndicator.textContent = '○ Offline';
       }
   } catch (error) {
-      document.getElementById('status-indicator-1').innerHTML = '○';
-      document.getElementById('status-indicator-1').classList.add('emoji');
-      document.getElementById('status-indicator-2').textContent = 'Error: Unable to reach server';
+      document.getElementById('status-indicator').innerHTML = '○';
+      document.getElementById('status-indicator').classList.add('emoji');
+      document.getElementById('status-indicator').textContent = 'Error: Unable to reach server';
     
   }
 }
 
 window.onload = checkServerStatus;
+
+/////////////////////////////
