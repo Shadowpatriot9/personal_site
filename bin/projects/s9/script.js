b55ab758196409fc813a2443e00d587759f65656
Shadowpatriot9 <gscovil9@gmail.com>
2024-12-23 05:37:17 -0700

    -emergency commit, fixed broken script to index reference link for s9

--- bin/projects/s9/script.js
+++ bin/projects/s9/script.js
@@ -6,7 +6,7 @@
 document.getElementById('gs-btn').addEventListener('click', function () {
     const mainContent = document.getElementById('overlay');
     window.location.href = '../../index.html';
-  });
+});
 
 /////////////////////////////
 // Functions | Monitoring Dashboard
