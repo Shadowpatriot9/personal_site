eafa2c661c6143bea49b64dedab3763d236e4ad1
Shadowpatriot9 <gscovil9@gmail.com>
2024-12-25 09:23:23 -0700

    -made templates for new projects -adjusted main text -minor text edit to s9

---
+++ bin/templates/script.js
@@ -0,0 +1,26 @@
+/////////////////////////////////////
+// Animations 
+/////////////////////////////////////
+
+
+
+/////////////////////////////
+// Buttons
+/////////////////////////////
+
+// GS
+document.getElementById('***insert name here***-btn').addEventListener('click', function () {
+    const mainContent = document.getElementById('overlay');
+    mainContent.classList.add('fade-out');
+    setTimeout(() => {
+      window.location.href = '../../index.html';
+    }, 500);
+  });
+
+/////////////////////////////
+// Functions
+/////////////////////////////
+
+
+
+/////////////////////////////
