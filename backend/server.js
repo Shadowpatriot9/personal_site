b5b3db27022e98f50113e9efe75d0ea24a79e016
Shadowpatriot9 <gscovil9@gmail.com>
2025-01-09 22:29:01 -0700

    -mongo db creation and integration to site

--- backend/server.js
+++ backend/server.js
@@ -15,3 +15,14 @@ const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.log('Error connecting to MongoDB:', err));
