d57811a9e9391192995d5f6697be28189dd345fe
Shadowpatriot9 <gscovil9@gmail.com>
2025-01-13 17:50:24 -0700

    -repush index for deployment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ function App() {
     return (
         <div style={{ textAlign: 'center', padding: '50px' }}>
             <h1>Welcome to MERN</h1>
-            <p>This is a minimal MERN setup.</p>
+            <p>This is a test MERN setup.</p>
         </div>
     );
 }
