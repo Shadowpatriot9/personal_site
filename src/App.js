805fd402ccd87bbfb652cca281ee1e0d49d5591c
Shadowpatriot9 <gscovil9@gmail.com>
2025-01-21 21:12:26 -0700

    -prototyping subpages w/mock main and subpage

--- src/App.js
+++ src/App.js
@@ -1,146 +1,179 @@
-import { useEffect } from 'react';
-import { initializeAnimations } from './script';
+// import { useEffect } from 'react';
+// import { initializeAnimations } from './script';
+// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+// import './styles/styles_page.css';
+// import './styles/styles_projects.css';
+// import './styles/styles_mobile.css';
+
+// import S9 from './projects/s9/s9.js';
+
+// // function App() {
+// //   useEffect(() => {
+// //     initializeAnimations();
+// //     return () => {
+// //       // Clean up any potential event listeners or animations here if needed
+// //     };
+// //   }, []);
+
+// //   return (
+// //     <Router>
+// //       <div className="App">
+// //         <div>
+// //         <Routes>
+// //           <Route path="/projects/s9" element={<S9 />} />
+// //         </Routes>
+// //           {/* Page Head */}
+// //           {/* Encoding */}
+// //           <meta charSet="UTF-8" />
+// //           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+// //           {/* Site Tab Title */}
+// //           <title> GS </title> 
+// //           {/* External Links */}
+// //           <link rel="stylesheet" href="./styles/styles_page.css" />
+// //           <link rel="stylesheet" href="./styles/styles_mobile.css" />
+// //           <link rel="stylesheet" href="./styles/styles_projects.css" />
+// //           {/* Page Body */}
+// //           {/* Home Splash Screen */}
+// //           <div className="splash-screen" id="splash-screen"> 
+// //             <div className="splash-logo">
+// //               Grayden Scovil
+// //               <div id="splash-overlay"> </div>
+// //             </div> 
+// //           </div>
+// //           {/* Fade Out Overlay */}
+// //           <div className="overlay" id="overlay" />
+// //           {/* Page Title */}
+// //           <header className="header"> 
+// //             <h1 id="main-title"> GS </h1>
+// //             <h1 id="full-name"> Grayden Scovil </h1>
+// //           </header>
+// //           {/* Main */}
+// //           <main> 
+// //             {/* Home Grid */}
+// //             <div className="grid-1">
+// //               {/* About Section */}
+// //               <section className="section" id="about"> 
+// //                 <h1 className="section-header" id="about"> About </h1> 
+// //                 <p>
+// //                   Welcome welcome. 😁 <br />
+// //                   To keep it simple, I'm Grayden and I'm in Colorado. 
+// //                   <br /> 
+// //                   <br />
+// //                   Feel free to check out my stuff below and click on the fancy stuff in the 'Contact' section 
+// //                   if want to see more of me.
+// //                 </p> 
+// //               </section>
+// //               {/* Contact Section */}
+// //               <section className="section" id="contact"> 
+// //                 <h1 className="section-header" id="contact"> Contact </h1> 
+// //                 <div className="contact-info">
+// //                   <p> Email: gscovil9@gmail.com </p> 
+// //                   <p> LinkedIn: <a href="https://www.linkedin.com/in/gscovil/" target="_blank"> linkedin.com/in/gscovil </a></p> 
+// //                   <p> GitHub: <a href="https://github.com/Shadowpatriot9" target="_blank"> github.com/Shadowpatriot9 </a> 
+// //                   </p></div>
+// //               </section>
+// //             </div>
+// //             <div className="grid-2">
+// //               {/* Projects Section */}
+// //               <section className="section" id="projects"> 
+// //                 <h1 className="section-header" id="projects"> Projects </h1> 
+// //                 <div className="projects-grid">
+// //                   {/* Project Card | S9QL */}
+// //                   <button className="card" id="s9ql-btn">
+// //                     <h3> S_Im </h3> 
+// //                     <p> Shadow Simulator (tbd) </p>
+// //                   </button> 
+// //                   {/* Project Card | sOS */}
+// //                   <button className="card" id="sos-btn">
+// //                     <h3> sOS </h3> 
+// //                     <p> Shadow Operating System </p>
+// //                   </button>
+// //                   {/* Project Card | S9 */}
+// //                   <Link to="./projects/s9/s9">
+// //                     <button className="card" id="s9-btn">
+// //                       <h3>S9</h3>
+// //                       <p>Shadow Home Server</p>
+// //                     </button>
+// //                   </Link>
+
+// //                   {/* Project Card | NFI */}
+// //                   <button className="card" id="nfi-btn">
+// //                     <h3> NFI </h3> 
+// //                     <p> (old, incomplete) <br /> Nuclear Fission-Based <br /> Propulsion System </p>
+// //                   </button>
+// //                   {/* Project Card | Muse */}
+// //                   <button className="card" id="muse-btn">
+// //                     <h3> Muse </h3> 
+// //                     <p> (old, incomplete) <br /> Automated Audio Equalizer </p>
+// //                   </button> 
+// //                   {/* Project Card | EyeLearn */}
+// //                   <button className="card" id="el-btn">
+// //                     <h3> EyeLearn </h3> 
+// //                     <p> (old, incomplete) <br /> Academia AR/VR Headset </p>
+// //                   </button>
+// //                   {/* Project Card | Naton */}
+// //                   <button className="card" id="naton-btn">
+// //                     <h3> Naton </h3> 
+// //                     <p> (old, incomplete) <br /> Element Converter </p>
+// //                   </button>
+// //                   {/* *** Project Card Template *** */}
+// //                   {/* <button class="card" id="***name***-btn">
+// //                               <h3> ***title*** </h3> 
+// //                               <p> ***text*** </p>
+// //                           </button> */}
+// //                 </div>
+// //               </section>
+// //             </div>
+// //           </main>
+// //           {/* Footer */}
+// //           <footer className="footer"> 
+// //             {/* Copyright */}
+// //             <div className="graphic">
+// //               <p> © 2024 Grayden Scovil </p> 
+// //             </div>
+// //             {/* Love Note */}
+// //             <div className="love-note">
+// //               <p> Made with ♥ </p>
+// //             </div>        
+// //           </footer>
+// //         </div>
+// //       </div> 
+// //     </Router>
+// //   );
+// // }
+
+// export default App;
+
+
+
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-
-import './styles/styles_page.css';
-import './styles/styles_projects.css';
-import './styles/styles_mobile.css';
-
-import S9 from './projects/s9/s9.js';
+import SubPage from './SubPage';
 
 function App() {
-  useEffect(() => {
-    initializeAnimations();
-    return () => {
-      // Clean up any potential event listeners or animations here if needed
-    };
-  }, []);
-
   return (
     <Router>
-      <div className="App">
-        <div>
-        <Routes>
-          <Route path="/projects/s9" element={<S9 />} />
-        </Routes>
-          {/* Page Head */}
-          {/* Encoding */}
-          <meta charSet="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          {/* Site Tab Title */}
-          <title> GS </title> 
-          {/* External Links */}
-          <link rel="stylesheet" href="./styles/styles_page.css" />
-          <link rel="stylesheet" href="./styles/styles_mobile.css" />
-          <link rel="stylesheet" href="./styles/styles_projects.css" />
-          {/* Page Body */}
-          {/* Home Splash Screen */}
-          <div className="splash-screen" id="splash-screen"> 
-            <div className="splash-logo">
-              Grayden Scovil
-              <div id="splash-overlay"> </div>
-            </div> 
-          </div>
-          {/* Fade Out Overlay */}
-          <div className="overlay" id="overlay" />
-          {/* Page Title */}
-          <header className="header"> 
-            <h1 id="main-title"> GS </h1>
-            <h1 id="full-name"> Grayden Scovil </h1>
-          </header>
-          {/* Main */}
-          <main> 
-            {/* Home Grid */}
-            <div className="grid-1">
-              {/* About Section */}
-              <section className="section" id="about"> 
-                <h1 className="section-header" id="about"> About </h1> 
-                <p>
-                  Welcome welcome. 😁 <br />
-                  To keep it simple, I'm Grayden and I'm in Colorado. 
-                  <br /> 
-                  <br />
-                  Feel free to check out my stuff below and click on the fancy stuff in the 'Contact' section 
-                  if want to see more of me.
-                </p> 
-              </section>
-              {/* Contact Section */}
-              <section className="section" id="contact"> 
-                <h1 className="section-header" id="contact"> Contact </h1> 
-                <div className="contact-info">
-                  <p> Email: gscovil9@gmail.com </p> 
-                  <p> LinkedIn: <a href="https://www.linkedin.com/in/gscovil/" target="_blank"> linkedin.com/in/gscovil </a></p> 
-                  <p> GitHub: <a href="https://github.com/Shadowpatriot9" target="_blank"> github.com/Shadowpatriot9 </a> 
-                  </p></div>
-              </section>
-            </div>
-            <div className="grid-2">
-              {/* Projects Section */}
-              <section className="section" id="projects"> 
-                <h1 className="section-header" id="projects"> Projects </h1> 
-                <div className="projects-grid">
-                  {/* Project Card | S9QL */}
-                  <button className="card" id="s9ql-btn">
-                    <h3> S_Im </h3> 
-                    <p> Shadow Simulator (tbd) </p>
-                  </button> 
-                  {/* Project Card | sOS */}
-                  <button className="card" id="sos-btn">
-                    <h3> sOS </h3> 
-                    <p> Shadow Operating System </p>
-                  </button>
-                  {/* Project Card | S9 */}
-                  <Link to="./projects/s9/s9">
-                    <button className="card" id="s9-btn">
-                      <h3>S9</h3>
-                      <p>Shadow Home Server</p>
-                    </button>
-                  </Link>
-
-                  {/* Project Card | NFI */}
-                  <button className="card" id="nfi-btn">
-                    <h3> NFI </h3> 
-                    <p> (old, incomplete) <br /> Nuclear Fission-Based <br /> Propulsion System </p>
-                  </button>
-                  {/* Project Card | Muse */}
-                  <button className="card" id="muse-btn">
-                    <h3> Muse </h3> 
-                    <p> (old, incomplete) <br /> Automated Audio Equalizer </p>
-                  </button> 
-                  {/* Project Card | EyeLearn */}
-                  <button className="card" id="el-btn">
-                    <h3> EyeLearn </h3> 
-                    <p> (old, incomplete) <br /> Academia AR/VR Headset </p>
-                  </button>
-                  {/* Project Card | Naton */}
-                  <button className="card" id="naton-btn">
-                    <h3> Naton </h3> 
-                    <p> (old, incomplete) <br /> Element Converter </p>
-                  </button>
-                  {/* *** Project Card Template *** */}
-                  {/* <button class="card" id="***name***-btn">
-                              <h3> ***title*** </h3> 
-                              <p> ***text*** </p>
-                          </button> */}
-                </div>
-              </section>
-            </div>
-          </main>
-          {/* Footer */}
-          <footer className="footer"> 
-            {/* Copyright */}
-            <div className="graphic">
-              <p> © 2024 Grayden Scovil </p> 
-            </div>
-            {/* Love Note */}
-            <div className="love-note">
-              <p> Made with ♥ </p>
-            </div>        
-          </footer>
-        </div>
-      </div> 
+      <div>
+        <header>
+          <h1>Main Page</h1>
+          <nav>
+            <Link to="/">Home</Link> | <Link to="/subpage">Go to SubPage</Link>
+          </nav>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/subpage" element={<SubPage />} />
+          </Routes>
+        </main>
+      </div>
     </Router>
   );
+}
+
+function Home() {
+  return <h2>Welcome to the Main Page!</h2>;
 }
 
 export default App;
--- src/projects/s9/s9.js
+++ src/projects/s9/s9.js
@@ -1,94 +1,94 @@
-import React, { useEffect } from 'react';
-// import { initializeAnimations } from './script';
-// import './projects/s9/styles.css';
+// import React, { useEffect } from 'react';
+// // import { initializeAnimations } from './script';
+// // import './projects/s9/styles.css';
 
-const S9 = () => {
-  // useEffect(() => {
-  //   if (document.readyState === 'complete' || document.readyState === 'interactive') {
-  //     initializeAnimations();
-  //   } else {
-  //     document.addEventListener('DOMContentLoaded', initializeAnimations);
-  //   }
-  //   return () => {
-  //     document.removeEventListener('DOMContentLoaded', initializeAnimations);
-  //   };
-  // }, []);
+// const S9 = () => {
+//   // useEffect(() => {
+//   //   if (document.readyState === 'complete' || document.readyState === 'interactive') {
+//   //     initializeAnimations();
+//   //   } else {
+//   //     document.addEventListener('DOMContentLoaded', initializeAnimations);
+//   //   }
+//   //   return () => {
+//   //     document.removeEventListener('DOMContentLoaded', initializeAnimations);
+//   //   };
+//   // }, []);
 
-  return (
-    <div>
-      {/* Header */}
-      <header id="header">
-        <button id="gs-btn">GS</button>
-      </header>
+//   return (
+//     <div>
+//       {/* Header */}
+//       <header id="header">
+//         <button id="gs-btn">GS</button>
+//       </header>
 
-      {/* Main Content */}
-      <main>
-        <div className="container">
-          {/* Top Grid */}
-          <div className="grid-1">
-            <section id="title">
-              <h1>S9</h1>
-            </section>
-            <section className="section" id="brief">
-              <h3>Description:</h3>
-              <p>(Date of origin: 10/2024)</p>
-              <p>
-                This is some info about my home server. I mainly use it for redundancy to my PAN and in specific cases to allow
-                more integration all around for files or other client-to-client connections. Trying to develop it further as
-                an all-around hosting platform idea for software development purposes, but still working out the kinks
-                to optimize the development workflow on it. Feel free to check her out below. 😊
-              </p>
-            </section>
-          </div>
+//       {/* Main Content */}
+//       <main>
+//         <div className="container">
+//           {/* Top Grid */}
+//           <div className="grid-1">
+//             <section id="title">
+//               <h1>S9</h1>
+//             </section>
+//             <section className="section" id="brief">
+//               <h3>Description:</h3>
+//               <p>(Date of origin: 10/2024)</p>
+//               <p>
+//                 This is some info about my home server. I mainly use it for redundancy to my PAN and in specific cases to allow
+//                 more integration all around for files or other client-to-client connections. Trying to develop it further as
+//                 an all-around hosting platform idea for software development purposes, but still working out the kinks
+//                 to optimize the development workflow on it. Feel free to check her out below. 😊
+//               </p>
+//             </section>
+//           </div>
 
-          {/* Mid Grid */}
-          <div className="grid-2">
-            <section className="section" id="features">
-              <h3>Features:</h3>
-              <ul>
-                <li>Backup NAS File System (NAS via NFS)</li>
-                <li>VM Hosting (Virtual Box w/specific OS imaged to preference)</li>
-                <li>Accessible Anywhere (SSH and AnyDesk for RDP)</li>
-                <li>Monitoring (HTOP, WireShark, Prometheus)</li>
-                <li>Auto Config Script, Auto Install ISO for re-imaging purposes</li>
-              </ul>
-            </section>
-            <section className="section" id="specs">
-              <h3>Specifications:</h3>
-              <p>OS: Ubuntu Server w/GNOME Desktop</p>
-              <p>CPU: i7-13700K</p>
-              <p>RAM: 32GB DDR5 5600</p>
-              <p>Storage: 1TB NVMe</p>
-              <p>Secret Name: Sally</p>
-            </section>
-          </div>
+//           {/* Mid Grid */}
+//           <div className="grid-2">
+//             <section className="section" id="features">
+//               <h3>Features:</h3>
+//               <ul>
+//                 <li>Backup NAS File System (NAS via NFS)</li>
+//                 <li>VM Hosting (Virtual Box w/specific OS imaged to preference)</li>
+//                 <li>Accessible Anywhere (SSH and AnyDesk for RDP)</li>
+//                 <li>Monitoring (HTOP, WireShark, Prometheus)</li>
+//                 <li>Auto Config Script, Auto Install ISO for re-imaging purposes</li>
+//               </ul>
+//             </section>
+//             <section className="section" id="specs">
+//               <h3>Specifications:</h3>
+//               <p>OS: Ubuntu Server w/GNOME Desktop</p>
+//               <p>CPU: i7-13700K</p>
+//               <p>RAM: 32GB DDR5 5600</p>
+//               <p>Storage: 1TB NVMe</p>
+//               <p>Secret Name: Sally</p>
+//             </section>
+//           </div>
 
-          {/* Bot Grid */}
-          <div className="grid-3">
-            <section className="section" id="monitor">
-              <h3>Monitoring Dashboard</h3>
-              <div id="status-container">
-                <p>Status:</p>
-                <div id="status-indicator"></div>
-              </div>
-              <p>Network Traffic:</p>
-              <canvas id="myChart"></canvas>
-            </section>
-          </div>
-        </div>
-      </main>
+//           {/* Bot Grid */}
+//           <div className="grid-3">
+//             <section className="section" id="monitor">
+//               <h3>Monitoring Dashboard</h3>
+//               <div id="status-container">
+//                 <p>Status:</p>
+//                 <div id="status-indicator"></div>
+//               </div>
+//               <p>Network Traffic:</p>
+//               <canvas id="myChart"></canvas>
+//             </section>
+//           </div>
+//         </div>
+//       </main>
 
-      {/* Footer */}
-      <footer>
-        <div className="graphic">
-          <p>© 2024 Grayden Scovil</p>
-        </div>
-        <div className="love-note">
-          <p>Made with ♥</p>
-        </div>
-      </footer>
-    </div>
-  );
-};
+//       {/* Footer */}
+//       <footer>
+//         <div className="graphic">
+//           <p>© 2024 Grayden Scovil</p>
+//         </div>
+//         <div className="love-note">
+//           <p>Made with ♥</p>
+//         </div>
+//       </footer>
+//     </div>
+//   );
+// };
 
-export default S9;
+// export default S9;
