978eec63a54095a4289fb5c2d1217191cd27a585
Shadowpatriot9 <gscovil9@gmail.com>
2025-01-21 23:09:09 -0700

    -added EL and NFI for creation and links -made templates for sub pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './main';
 import S9 from './projects/s9/s9';
 import Muse from './projects/muse/muse';
+import EL from './projects/eye-learn/EL';
+import NFI from './projects/nfi/NFI';
+// **template** import name_of_page_ from './projects/**/**';
 
 function App() {
   return (
@@ -13,6 +16,10 @@ function App() {
           <Route path="/" element={<Main />} />
           <Route path="/projects/s9" element={<S9 />} />
           <Route path="/projects/muse" element={<Muse />} />
+          <Route path="/projects/EL" element={<EL />} />
+          <Route path="/projects/NFI" element={<NFI />} />
+          {/* **template** <Route path="/projects/**" element={<name_of_page_ />} /> */}
+
         </Routes>
       </div>
     </Router>
